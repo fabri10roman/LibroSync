33635b03379f86410dc02c6e37543051489f7ca9
fabri10roman <fabri11roman.fr@gmail.com>
2024-01-30 17:38:54 -0300

    init

---
+++ src/main/java/com/demo/LibroSync/LibroSyncApplication.java
@@ -0,0 +1,13 @@
+package com.demo.LibroSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/LibroSync/LibroSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.LibroSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
