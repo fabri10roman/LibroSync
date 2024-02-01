27a01c28a2937da7b52a514376d71cde6d620140
fabri10roman <fabri11roman.fr@gmail.com>
2024-02-01 19:19:27 -0300

    Add GlobalExceptionHandler and handler for IllegalStateException

---
+++ src/main/java/com/demo/LibroSync/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.demo.LibroSync.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<?> handleIllegalStateException(IllegalStateException e){
+        ExceptionPayload exceptionPayload = ExceptionPayload.builder()
+                .message(e.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .title(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .build();
+        return new ResponseEntity<>(exceptionPayload, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
