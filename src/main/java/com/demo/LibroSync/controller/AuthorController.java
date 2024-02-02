945975de12d7befd166f41393ed76e040813ad0f
fabri10roman <fabri11roman.fr@gmail.com>
2024-02-02 15:45:20 -0300

    Author 'getAll' endpont

--- src/main/java/com/demo/LibroSync/controller/AuthorController.java
+++ src/main/java/com/demo/LibroSync/controller/AuthorController.java
@@ -4,10 +4,7 @@ import com.demo.LibroSync.domain.Author;
 import com.demo.LibroSync.dto.ResponsePayload;
 import com.demo.LibroSync.service.AuthorService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/authors")
@@ -20,6 +17,11 @@ public class AuthorController {
     public ResponsePayload create(@RequestBody Author author) {
         return authorService.create(author);
     }
+    @GetMapping
+    public ResponsePayload getAll() {
+        return authorService.getAll();
+    }
+
 
 
 }
