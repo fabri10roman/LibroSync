3b867a8b7928634edc71fa451e45d67aedf044f1
fabri10roman <fabri11roman.fr@gmail.com>
2024-02-02 21:25:10 -0300

    Add exception handler for BadRequestException

--- src/main/java/com/demo/LibroSync/exception/GlobalExceptionHandler.java
+++ src/main/java/com/demo/LibroSync/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.demo.LibroSync.exception;
 
 
 import com.demo.LibroSync.dto.ExceptionPayload;
+import org.apache.coyote.BadRequestException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,5 +37,14 @@ public class GlobalExceptionHandler {
                 .title(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
                 .build();
         return new ResponseEntity<>(exceptionPayload, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handleBadRequestException(BadRequestException e){
+        ExceptionPayload exceptionPayload = ExceptionPayload.builder()
+                .message(e.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .build();
+        return new ResponseEntity<>(exceptionPayload, HttpStatus.BAD_REQUEST);
     }
 }
